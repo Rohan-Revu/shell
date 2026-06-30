d4a5366d308eb114eaea16ffafd8a6150e92254a
Rohan-Revu <rohan.charandeeprevu@gmail.com>
2026-06-30 21:42:25 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,43 @@
 #include <string>
 #include <unordered_set>
 
+
+void typeCommand(std::string input){
+  std::string type_check = input.substr(5);
+  std::unordered_set<std::string> commands = {
+    "echo",
+    "exit",
+    "type"
+  };
+  bool found = false;
+
+  if(commands.contain(type_check)){
+    found = true;
+    std::cout << type_check << " is a shell builtin" << std::endl;
+    break
+  }
+  if(!found){
+    std::string path = getenv("PATH");
+    std::istringstream ss(path);
+    std::string dictionary;
+
+    while(getline(ss, dictionary, ':')){
+      std::string full_path = dictionary + "/" + type_check;
+
+      if(!access(full_path.c_str(), X_OK)){
+        found = true;
+        std::cout << type_check << " is" << full_path << std::endl;
+        return;
+      }
+    }
+    std::cout << type_check << ": not found" << std::endl;
+
+    return;
+  }
+
+}
+
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -25,20 +62,7 @@ int main() {
       std::cout << input.substr(5) << std::endl;
     }
     else if(command == "type"){
-      std::string type_check = input.substr(5);
-      std::unordered_set<std::string> commands = {
-        "echo",
-        "exit",
-        "pwd",
-        "type"
-      };
-
-      if(commands.contains(type_check)){
-        std::cout << type_check << " is a shell builtin" << std::endl;
-      }
-      else{
-        std::cout << tyep_check << ": not found" << std::endl;
-      }
+      typeCommand(input);
     }
     else{
       std::cout << command << ": command not found" << std::endl;
