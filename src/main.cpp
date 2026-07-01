198679ca2a6281355cb6670191ac3e789eb2ac59
Rohan-Revu <rohan.charandeeprevu@gmail.com>
2026-07-01 12:53:11 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -104,39 +104,102 @@ std::vector<std::string> parseArguments(const std::string &input) {
     bool inSingleQuote = false;
     bool inDoubleQuote = false;
 
-    for (int i=0; i < input.size(); i++) {
+    auto pushCurrent = [&]() {
+        if (!current.empty()) {
+            args.push_back(current);
+            current.clear();
+        }
+    };
+
+    for (size_t i = 0; i < input.size(); i++) {
         char c = input[i];
 
+        // Backslash
+        if (c == '\\' && !inSingleQuote) {
+            if (i + 1 < input.size()) {
+                current += input[++i];
+            }
+            continue;
+        }
 
-        if(c=='\\' && !inSingleQuote){ 
-          if(i + 1 < input.size()){
-            current += input[i + 1];
-            i++;
-          }
-        }
-        else if (c == '\'' && !inDoubleQuote) {
+        // Single quotes
+        if (c == '\'' && !inDoubleQuote) {
             inSingleQuote = !inSingleQuote;
+            continue;
         }
-        else if (c == '"' && !inSingleQuote) {
+
+        // Double quotes
+        if (c == '"' && !inSingleQuote) {
             inDoubleQuote = !inDoubleQuote;
+            continue;
         }
-        else if (c == ' ' && !inSingleQuote && !inDoubleQuote) {
-            if (!current.empty()) {
-                args.push_back(current);
-                current.clear();
+
+        // Redirection operators (only outside quotes)
+        if (!inSingleQuote && !inDoubleQuote) {
+
+            // 1>>
+            if (i + 2 < input.size() &&
+                input.substr(i, 3) == "1>>") {
+                pushCurrent();
+                args.push_back("1>>");
+                i += 2;
+                continue;
+            }
+
+            // 2>>
+            if (i + 2 < input.size() &&
+                input.substr(i, 3) == "2>>") {
+                pushCurrent();
+                args.push_back("2>>");
+                i += 2;
+                continue;
+            }
+
+            // >>
+            if (i + 1 < input.size() &&
+                input.substr(i, 2) == ">>") {
+                pushCurrent();
+                args.push_back(">>");
+                i++;
+                continue;
+            }
+
+            // 1>
+            if (i + 1 < input.size() &&
+                input.substr(i, 2) == "1>") {
+                pushCurrent();
+                args.push_back("1>");
+                i++;
+                continue;
+            }
+
+            // 2>
+            if (i + 1 < input.size() &&
+                input.substr(i, 2) == "2>") {
+                pushCurrent();
+                args.push_back("2>");
+                i++;
+                continue;
+            }
+
+            // >
+            if (c == '>') {
+                pushCurrent();
+                args.push_back(">");
+                continue;
+            }
+
+            // Whitespace
+            if (isspace(static_cast<unsigned char>(c))) {
+                pushCurrent();
+                continue;
             }
         }
-        else {
-            current += c;
-        }
-    }
-    for (const auto &arg : args) {
-      std::cerr << "[" << arg << "]";
-    }
-    std::cerr << '\n';
 
-    if (!current.empty())
-        args.push_back(current);
+        current += c;
+    }
+
+    pushCurrent();
 
     return args;
 }
