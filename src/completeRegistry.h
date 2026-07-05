37acd454e9b1178ea0fbe190174b3afd341d5b84
Rohan-Revu <rohan.charandeeprevu@gmail.com>
2026-07-05 23:49:20 +0530

    codecrafters submit [skip ci]

--- src/completeRegistry.h
+++ src/completeRegistry.h
@@ -8,4 +8,4 @@ void registerCompleter(const std::string& command, const std::string& completer)
 
 std::string getCompleter(const std::string& command);
 
-std::vector<std::string> runCompleter(const std::string& command);
+std::vector<std::string> runCompleter(const std::string& command)
