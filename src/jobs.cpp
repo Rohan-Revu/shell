91fc70e5d93bd4e82185ae9400c3a7dd2619fe7a
Rohan-Revu <rohan.charandeeprevu@gmail.com>
2026-07-06 23:27:37 +0530

    codecrafters submit [skip ci]

--- src/jobs.cpp
+++ src/jobs.cpp
@@ -11,26 +11,24 @@ static int nextJobNumber = 1;
 
 int addJob(pid_t pid, const std::string& command)
 {
-    jobs.push_back({nextJobNumber++, pid, command, "Running"});
+    jobs.push_back({nextJobNumber++, pid, command, JobStatus::Running});
     return jobs.back().jobNumber;
 }
 
-void printJobs()
-{
+void printJobs() {
     for (auto& job : jobs){
         int status;
         pid_t result = waitpid(job.pid, &status, WNOHANG);
 
         if (result == job.pid && WIFEXITED(status))
         {
-            job.status = "Done";
+            job.status = JobStatus::Done;
         }
     }
 
-
-    for (size_t i = 0; i < jobs.size(); i++)
-    {
+    for (size_t i = 0; i < jobs.size(); i++){
         auto& job = jobs[i];
+
         char marker = ' ';
 
         if (i == jobs.size() - 1)
@@ -38,35 +36,32 @@ void printJobs()
         else if (jobs.size() >= 2 && i == jobs.size() - 2)
             marker = '-';
 
-        std::cout << "[" << job.jobNumber << "]" << marker << "  " << std::left << std::setw(24) << job.status;
+        std::cout << "[" << job.jobNumber << "]"
+                  << marker << "  ";
 
-        if (job.status == "Running")
-        {
-            std::cout << job.command;
+        if (job.status == JobStatus::Running){
+            std::cout << std::left << std::setw(24) << "Running" << job.command;
         }
-        else
-        {
+        else{
             std::string command = job.command;
 
-            if (command.size() >= 2 &&
-                command.substr(command.size() - 2) == " &")
-            {
+            if (command.size() >= 2 && command.substr(command.size() - 2) == " &") {
                 command.erase(command.size() - 2);
             }
 
-            std::cout << command;
+            std::cout << std::left << std::setw(24) << "Done" << command;
         }
-
         std::cout << std::endl;
     }
 
+    // Remove completed jobs
     jobs.erase(
         std::remove_if(
             jobs.begin(),
             jobs.end(),
             [](const Job& job)
             {
-                return job.status == "Done";
+                return job.status == JobStatus::Done;
             }),
         jobs.end());
 }
