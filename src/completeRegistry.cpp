78293d1f881a93146e35f47f5ffdb77ab0acee09
Rohan-Revu <rohan.charandeeprevu@gmail.com>
2026-07-06 12:28:56 +0530

    codecrafters submit [skip ci]

--- src/completeRegistry.cpp
+++ src/completeRegistry.cpp
@@ -93,5 +93,7 @@ std::vector<std::string> runCompleter(const std::string& command, const std::str
     //close the read end of the pipe
     close(pipefd[0]);
 
+
+    std::sort(result.begin(), result.end());
     return result;
 }
