dea40cb1363c613110485a67302689cbd9e7af59
Rohan-Revu <rohan.charandeeprevu@gmail.com>
2026-07-06 23:03:46 +0530

    codecrafters submit [skip ci]

--- src/commandExecutor.h
+++ src/commandExecutor.h
@@ -3,4 +3,4 @@
 #include <vector>
 #include <string>
 
-bool executeCommand(const std::vector<std::string>& args, bool background, const std::string& commandLine);
+bool executeCommand(const std::vector<std::string>& args, bool background, const std::string& command);
